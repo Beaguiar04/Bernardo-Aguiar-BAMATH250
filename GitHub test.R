3e61b2f14a0e7a38da468f724d89cceb49bf22a1
Bernardo Aguiar <bernardoaguiar@Bernardos-MacBook-Air-2.local>
2025-11-06 21:48:44 -0600

    Initial commit

---
+++ GitHub test.R
@@ -0,0 +1,14 @@
+library(usethis)
+
+# First register for a github account.
+
+create_github_token()
+gitcreds::gitcreds_set()
+
+# To push a git repo to GitHub:
+use_github()
+
+# I'm going to start a new project first.
+
+# Let's make a change or two. I'll add that ghost cartoon
+
